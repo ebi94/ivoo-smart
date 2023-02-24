09473cc366b79ea1016179d36f0197e821db5e8e
febriakbar <febri.triakbar@yahoo.com>
2023-02-24 17:28:09 +0700

    add image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,13 +7,15 @@ import CctvIndoorContent from "../../content/CctvIndoorContent.json";
 import ListContent from "../../content/ListContent.json";
 import PacketContent from "../../content/PacketContent.json";
 import ContactContent from "../../content/ContactContent.json";
+import { Row, Col } from "antd";
+import { Slide } from "react-awesome-reveal";
+import { MiddleBlockSection, Content, ContentWrapper } from "../../components/MiddleBlock/styles";
 
 const Contact = lazy(() => import("../../components/ContactForm"));
 const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
 const Container = lazy(() => import("../../common/Container"));
 const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 const ContentBlock = lazy(() => import("../../components/ContentBlock"));
-const PricingBlock = lazy(() => import("../../components/PricingBlock"));
 const WhatsappChatBlock = lazy(() => import("../../components/WhatsappChatBlock"));
 
 const Home = () => {
@@ -74,13 +76,21 @@ const Home = () => {
         widthIcon="400px"
         id="about"
       />
-      {/* <ContentBlock
-        type="left"
-        title={ProductContent.title}
-        content={ProductContent.text}
-        icon="waving.svg"
-        id="product"
-      /> */}
+      <MiddleBlockSection>
+        <Slide triggerOnce direction="up">
+          <Row justify="center" align="middle">
+            <ContentWrapper>
+              <Col lg={24} md={24} sm={24} xs={24}>
+                <h6>Promo</h6>
+                <Content>
+                  <img src={`/img/png/promo-1.jpeg`} alt="Promo" width={'auto'} height={780} style={{marginBottom: 50}} />
+                  <img src={`/img/png/promo-2.jpeg`} alt="Promo" width={'auto'} height={550} />
+                </Content>
+              </Col>
+            </ContentWrapper>
+          </Row>
+        </Slide>
+      </MiddleBlockSection>
       <Contact
         title={ContactContent.title}
         content={ContactContent.text}
